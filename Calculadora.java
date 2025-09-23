51768979ed792e403ab071e3eefaff642c681e6d
Iv8-9 <isvanna.010975@gmail.com>
2025-09-23 11:11:57 -0600

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -4,6 +4,7 @@ public class Calculadora {
         System.out.println("Resultado: " + resultado); 
     } 
  
-    public static int sumar(int a, int b) {         return a + b; 
+    public static int sumar(int a, int b) {   
+      return a + b; 
     } 
 }  
