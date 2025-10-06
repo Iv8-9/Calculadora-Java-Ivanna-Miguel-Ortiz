54106121efaeffda227f6cb8dded7692e69c314d
Iv8-9 <isvanna.010975@gmail.com>
2025-10-06 00:16:10 -0600

    pruebas

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,33 +1,29 @@
 package src.test.java;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import src.main.java.Calculadora;
 import src.main.java.Operaciones;
-   
-public class CalculadoraTest {
-   
-       @Test
-       public void testSumar() {
-           assertEquals(9, Calculadora.sumar(4, 5));
-       }
-   
-       @Test
-       public void testRestar() {
-           assertEquals(2, Calculadora.restar(5, 3)); 
-       }
-     
-       @Test
-       public void testMultiplicar() {
-           assertEquals(15, Calculadora.multiplicar(3, 5));
-       }
-   
-       @Test
-       public void testDividir() {
-           assertEquals(4, Calculadora.dividir(20, 5));
-       }
-   }
-   
-    
-}
 
+public class CalculadoraTest {
+
+    @Test
+    public void testSumar() {
+        assertEquals(9, Operaciones.sumar(4, 5), 0.001);
+    }
+
+    @Test
+    public void testRestar() {
+        assertEquals(2, Operaciones.restar(5, 3), 0.001); 
+    }
+	 
+    @Test
+    public void testMultiplicar() {
+        assertEquals(15, Operaciones.multiplicar(3, 5), 0.001);
+    }
+
+    @Test
+    public void testDividir() {
+        assertEquals(4, Operaciones.dividir(20, 5), 0.001);
+    }
+}
 
